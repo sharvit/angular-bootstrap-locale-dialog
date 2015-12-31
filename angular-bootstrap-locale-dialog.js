6b433ce571ab51c796bdb267a4f51ab82806004a
Avi Sharvit <sharvita@gmail.com>
2016-01-01 00:57:19 +0200

    v1.0.5

--- angular-bootstrap-locale-dialog.js
+++ angular-bootstrap-locale-dialog.js
@@ -1,6 +1,6 @@
 /**
  * angular-bootstrap-locale-dialog - Locale selector dialog for angular-bootstrap
- * @version v1.0.4
+ * @version v1.0.5
  * @link https://sharvit.github.io/angular-bootstrap-locale-dialog/
  * @license MIT
  */
@@ -120,7 +120,7 @@
 })();
 /**
  * angular-bootstrap-locale-dialog - Locale selector dialog for angular-bootstrap
- * @version v1.0.4
+ * @version v1.0.5
  * @link https://sharvit.github.io/angular-bootstrap-locale-dialog/
  * @license MIT
  */
