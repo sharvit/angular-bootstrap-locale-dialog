77732fc7dc8f743753bbd2ed68049d92f6b67cbe
Avi Sharvit <sharvita@gmail.com>
2016-01-01 12:11:52 +0200

    fix travis

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -7,10 +7,18 @@
     var plugins       = require('gulp-load-plugins')();
 
     gulp.task('deploy:gh-pages', ['build'], function() {
+        var options = {
+            remove: false
+        };
+
+        if (process.env.TRAVIS) {
+            console.log('deploy gh-pages with travis');
+
+            options.remoteUrl = 'https://' + process.env.GH_TOKEN + '@github.com/sharvit/angular-bootstrap-locale-dialog.git';
+        }
+
         return gulp.src('./dist/**/*')
-            .pipe(plugins.ghPages({
-                remove: false
-            }))
+            .pipe(plugins.ghPages(options))
         ;
     });
 })();
