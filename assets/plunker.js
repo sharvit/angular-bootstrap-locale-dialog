55e915e2168f56648e0c861f9af57c83d1fad902
Avi Sharvit <sharvita@gmail.com>
2016-01-02 21:39:08 +0000

    Update 2016-01-02T21:39:07.587Z

--- assets/plunker.js
+++ assets/plunker.js
@@ -2,7 +2,7 @@ angular.module('plunker', [])
 
   .factory('plunkGenerator', function ($document) {
 
-    return function (ngVersion, bsVersion, version, module, content) {
+    return function (nguiVersion, bsuiVersion, uiVersion, faVersion, fiVersion, version, content) {
 
       var form = angular.element('<form style="display: none;" method="post" action="http://plnkr.co/edit/?p=preview" target="_blank"></form>');
       var addField = function (name, value) {
@@ -11,15 +11,18 @@ angular.module('plunker', [])
         form.append(input);
       };
 
-      var indexContent = function (content, version) {
+      var indexContent = function (content) {
         return '<!doctype html>\n' +
-          '<html ng-app="ui.bootstrap.demo">\n' +
+          '<html ng-app="demo">\n' +
           '  <head>\n' +
-          '    <script src="//ajax.googleapis.com/ajax/libs/angularjs/'+ngVersion+'/angular.js"></script>\n' +
-          '    <script src="//ajax.googleapis.com/ajax/libs/angularjs/'+ngVersion+'/angular-animate.js"></script>\n' +
-          '    <script src="//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-'+version+'.js"></script>\n' +
+          '    <script src="//ajax.googleapis.com/ajax/libs/angularjs/'+nguiVersion+'/angular.js"></script>\n' +
+          '    <script src="//ajax.googleapis.com/ajax/libs/angularjs/'+nguiVersion+'/angular-animate.js"></script>\n' +
+          '    <script src="//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-'+uiVersion+'.js"></script>\n' +
+          '    <script src="//sharvit.github.io/angular-bootstrap-locale-dialog/angular-bootstrap-locale-dialog-v' + version + '.js"></script>\n' +
           '    <script src="example.js"></script>\n' +
-          '    <link href="//netdna.bootstrapcdn.com/bootstrap/'+bsVersion+'/css/bootstrap.min.css" rel="stylesheet">\n' +
+          '    <link href="//netdna.bootstrapcdn.com/bootstrap/'+bsuiVersion+'/css/bootstrap.min.css" rel="stylesheet">\n' +
+          '    <link href="//maxcdn.bootstrapcdn.com/font-awesome/'+faVersion+'/css/font-awesome.min.css" rel="stylesheet">\n' +
+          '    <link href="//cdnjs.cloudflare.com/ajax/libs/flag-icon-css/'+fiVersion+'/css/flag-icon.css" rel="stylesheet">\n' +
           '  </head>\n' +
           '  <body>\n\n' +
           content + '\n' +
@@ -28,11 +31,11 @@ angular.module('plunker', [])
       };
 
       var scriptContent = function(content) {
-        return "angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);" + "\n" + content;
+        return "angular.module('demo', ['ngAnimate', 'ui.bootstrap', 'ui.bootstrap.locale-dialog']);" + "\n" + content;
       };
 
-      addField('description', 'http://angular-ui.github.io/bootstrap/');
-      addField('files[index.html]', indexContent(content.markup, version));
+      addField('description', 'https://sharvit.github.io/angular-bootstrap-locale-dialog/');
+      addField('files[index.html]', indexContent(content.markup));
       addField('files[example.js]', scriptContent(content.javascript));
 
       $document.find('body').append(form);
@@ -45,15 +48,17 @@ angular.module('plunker', [])
 
     $scope.content = {};
 
-    $scope.edit = function (ngVersion, bsVersion, version, module) {
-      plunkGenerator(ngVersion, bsVersion, version, module, $scope.content);
+    $scope.edit = function (nguiVersion, bsuiVersion, uiVersion, faVersion, fiVersion, version) {
+      plunkGenerator(nguiVersion, bsuiVersion, uiVersion, faVersion, fiVersion, version, $scope.content);
     };
   })
 
-  .directive('plunkerContent', function () {
+  .directive('plunkerContent', function ($timeout) {
     return {
       link:function (scope, element, attrs) {
-        scope.content[attrs.plunkerContent] = element.text().trim();
+        $timeout(function () {
+          scope.content[attrs.plunkerContent] = element.text().trim();
+        }, 1000);
       }
     };
   });
